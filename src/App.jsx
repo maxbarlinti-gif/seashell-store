51a7b5184d1abcd016dd61f48d273167416d8a83
maxbarlinti-gif <maxbarlinti@gmail.com>
2026-09-21 00:52:00 +0700

    Replace shell with marketplace hero image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import marketplaceHero from './Everything You Need, All in One Place.png'
 const sampleProducts = [
   {
     id: 1,
@@ -68,9 +68,13 @@ export default function App() {
             </a>
           </div>
 
-          <div className="hero-art">
-            <div className="shell">🐚</div>
-          </div>
+         <div className="hero-art">
+  <img
+   src={marketplaceHero}
+    alt="Ardas Ong Marketplace products"
+    className="marketplace-hero-image"
+  />
+</div>
         </section>
 
         <section className="shop-section" id="shop">
